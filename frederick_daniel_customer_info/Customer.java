ed49f9314ed82d9cbe51e6cbe49c11e9904ebdfe
Daniel-Frederick <frederickd5@udayton.edu>
2024-02-10 21:05:15 -0500

    Adjustng arguments in abstract methods

--- frederick_daniel_customer_info/Customer.java
+++ frederick_daniel_customer_info/Customer.java
@@ -4,9 +4,9 @@ public abstract class Customer {
     
     // Abstract method for sorting customer IDs
     // Shell sort
-    abstract int[] sortCustomerIDs(int[] ids);
+    abstract int[] sortCustomerIDs(int[] ids, String[] names);
 
     // Abstract method for sorting customer names
     // Insertion Sort
-    abstract String[] sortCustomerNames(String[] names);
+    abstract String[] sortCustomerNames(int[] ids, String[] names);
 }
