7f4bbf44a9bd1a420e7090c4f2e3883c78594cba
Daniel-Frederick <frederickd5@udayton.edu>
2024-02-12 15:10:24 -0500

    Did some more testing

--- Resources/Testing.java
+++ Resources/Testing.java
@@ -1,6 +1,35 @@
 public class Testing {
 
     public static void main(String[] args) {
+
+        // Stacks testing 
+        Stacks stack = new Stacks(5);
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60); // Stack is full
+
+        // Top of the stack
+        System.out.println(stack.peek());
+
+        // remove elements from stack
+        System.out.println(stack.pop());
+
+        // Make a new method in Stacks to empty the stack
+        // Remove all elements from stack
+        while(!stack.isEmpty()) { // Check to see if the stack is empty
+            int value = stack.pop();
+            System.out.println(value);
+        }
+        stack.empty(stack);
+        System.out.println("stack is now emptied.");
+        
+        System.out.println(stack.size());
+
+        
         Stacks bucket = new Stacks(4);
 
         System.out.println(bucket.isEmpty());
@@ -29,6 +58,7 @@ public class Testing {
 
 
         System.out.println(bucket.isEmpty());
+        
     }
 
 }
