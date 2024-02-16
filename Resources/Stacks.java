7aaaf6b10c51545e4aac7569b58173adce021261
Daniel-Frederick <frederickd5@udayton.edu>
2024-02-16 13:32:06 -0500

    Adjusted Stacks class methods and comments

--- Resources/Stacks.java
+++ Resources/Stacks.java
@@ -17,79 +17,53 @@ empty - Removes all elements from stack
 
 //Skeleton class of a Java Stacks 
 public class Stacks {
-	// max size
-	private int maxSize;
+    private int[] stack; // Array to store stack elements
+    private int top; // Index of the top element in the stack
+    private int capacity; // Maximum capacity of the stack
 
-	//array will store elements of the stack
-	private int[] stackArray;
+    // Constructor to initialize the stack
+    public Stacks(int capacity) {
+        this.capacity = capacity;
+        stack = new int[capacity];
+        top = -1; // Stack is initially empty
+    }
 
-	// index of the top element of the stack
-	private int top;
-
-	// constructor
-	public Stacks(int size) {
-		this.maxSize = size;
-		this.stackArray = new int[maxSize];
-		// // stack is empty
-		this.top = -1;
-	}
-
-	// push elements into the stack
-	public void push(int value) {
-		// check if stack full
-		if(top  < maxSize - 1) {
-			//go to top and add value (++ increment)
-			stackArray[++top] = value;
-		}
-		else {
-			System.out.println("Stack is full, cannot push more values");
-		}
-	}
-
-	// pop elements 
-	public int pop() {
-		if(top >= 0) {
-			// return top value and decrement
-			return stackArray[top--];
-		}else {
-			System.out.println("Stack is empty, cannot pop values");
-		}
-		//return invalid value
-		return -1;
-	}
-
-	// method to check if stack empty or not
-	public int peek() {
-		// check if stack is not empty
-		if(top >=0) {
-			// Return the top element
-			return stackArray[top];
-		}else {
-			System.out.println("Stack is empty,unable to peek");
-		}
-		//return invalid value
-		return -1;
-	}
-
-	// check if the stack is completely empty
-	public boolean isEmpty() {
-		return (top == -1);
-
-	}
-
-	// check the size of the stack
-	public int size() {
-		return top + 1;
-	}
-
-
-	// Extra method I made
-	// Removes all elements from the stack
-	public void empty(Stacks stack) {
-		while(!stack.isEmpty()) { // Check to see if the stack is empty
-            int value = stack.pop();
-            System.out.println(value);
+    // Method to add an element to the stack
+    public void push(int item) {
+        if (top < capacity - 1) {
+            stack[++top] = item; // Increment top and add item
+        } else {
+            System.out.println("Stack is full");
         }
-	}
+    }
 
+    // Method to remove and return the top element of the stack
+    public int pop() {
+        if (top >= 0) {
+            return stack[top--]; // Return top item and decrement top
+        } else {
+            System.out.println("Stack is empty");
+            return -1; // Return an error value or throw an exception
+        }
+    }
+
+    // Method to return the top element of the stack without removing it
+    public int peek() {
+        if (top >= 0) {
+            return stack[top];
+        } else {
+            System.out.println("Stack is empty");
+            return -1; // Return an error value or throw an exception
+        }
+    }
+
+    // Method to check if the stack is empty
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    // Method to clear the stack
+    public void clear() {
+        top = -1; // Simply reset top to -1 to "clear" the stack
+    }
 }
